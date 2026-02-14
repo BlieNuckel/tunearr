d7f24b4c93b767c339a2ec34d656e7e602f239ca
Lasse Poulsen <lassekrp@pm.me>
2026-02-14 22:17:55 +0100

    extend config support

--- server/config.ts
+++ server/config.ts
@@ -6,36 +6,43 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 interface IConfig {
-  lidarrUrl: string;
-  lidarrApiKey: string;
+  lidarrUrl?: string;
+  lidarrApiKey?: string;
+  lidarrQualityProfileId?: number;
+  lidarrRootFolderPath?: string;
+  lidarrMetadataProfileId?: number;
 }
 
-const DEFAULT_CONFIG: IConfig = {
-  lidarrUrl: "",
-  lidarrApiKey: "",
-};
+const DEFAULT_CONFIG: IConfig = {};
 
 const CONFIG_DIR =
   process.env.APP_CONFIG_DIR || path.join(__dirname, "..", "config");
 const CONFIG_PATH = path.join(CONFIG_DIR, "config.json");
 
+const getConfig = (): IConfig => {
+  try {
+    const data = fs.readFileSync(CONFIG_PATH, "utf-8");
+    return { ...DEFAULT_CONFIG, ...JSON.parse(data) };
+  } catch {
+    return { ...DEFAULT_CONFIG };
+  }
+};
+
 const config = {
-  get: () => {
-    try {
-      const data = fs.readFileSync(CONFIG_PATH, "utf-8");
-      return { ...DEFAULT_CONFIG, ...JSON.parse(data) };
-    } catch {
-      return { ...DEFAULT_CONFIG };
-    }
+  get: (key: keyof IConfig) => {
+    return getConfig()[key];
   },
-  set: (config: IConfig) => {
+  set: <T extends keyof IConfig>(key: T, value: IConfig[T]) => {
     if (!fs.existsSync(CONFIG_DIR)) {
       console.log(`config directory missing. creating it in ${CONFIG_DIR}`);
       fs.mkdirSync(CONFIG_DIR, { recursive: true });
     }
 
-    fs.writeFileSync(CONFIG_PATH, JSON.stringify(config, null, 2));
+    const currentConfig = getConfig();
+    const newConfig = { ...currentConfig, [key]: value };
+
+    fs.writeFileSync(CONFIG_PATH, JSON.stringify(newConfig, null, 2));
   },
 };
 
-export { config, IConfig };;
+export { config, IConfig };
